f12aadf1744f022c5c1403a4fceea83164fbf573
Martin Bøgsted <jywh@aas.its.nja.dk>
2021-02-05 10:58:40 +0100

    Simulation as function.

--- R/KLDivergence.R
+++ R/KLDivergence.R
@@ -1,58 +1,62 @@
 library(tidyverse)
 
-kl <- function(dy, lam){
+# Double sampling of specific cluster
 
-  # Kullback-Leibler divergence between a discrete density dy, 
-  # and the Poisson distribution with parameter lm
+cluster.sampling <- function(N, xk, xkm, nk, mk, nsim = 100){
+
+  yk <- rep(0,nsim)
   
-  res <- 0
-  for(i in 1:length(dy[1,])){
-    res <- res + dy[2,i] * log(dy[2,i]/dpois(dy[1,i],lam))
+  for(i in 1:nsim){
+    x <- rep("Neg",N)
+    x[1:xk] <- "C"
+    x[(xk+1):(xk+xkm)] <- "Pos"
+    
+    z <- sample(x, nk)
+    
+    y <- sample(z[z!="Neg"], mk)
+    
+    yk[i] <- sum(y=="C")
+    
   }
-  res
+  yk
+}
+
+
+# Kullback-Leibler divergence between a discrete density dy 
+# and the Poisson distribution with parameter lamda
+
+kl <- function(dy, lambda){
+  
+  sum(dy[,2]*log(dy[,2]/dpois(dy[,1],lambda)))
 
 }
 
-N   <- 5000000                # Population size
-xkmm <- 0.015*N               # 1.5% of the population is positive
-xk  <- round(0.015*0.003*N)   # 0.3% of infected with specific variant
+
+# Read sequence data from the Danish Cluster 5 case
+
+seqdata <- read.csv("Data/rn_cluster5_data.csv", sep = ";")
+
+N   <- 600000                 # Population size (North Denmark Region)
+xkmm <- 0.006*N               # 0.6% of the population is positive (median)
+xk  <- round(0.006*N*0.08)    # 8% of infected with specific variant
 xkm <- xkmm - xk              # Number of infected with non-specific variant
 
-nk  <- 350000   # Numer of PCR tests
-mk  <- round(0.2*nk*(xk+xkm)/N)  # 0.2 of positive sent for WGS tests
+nk  <- 17000   # Numer of PCR tests (median)
+mk  <- round(0.28*nk*(xk+xkm)/N)  # 0.28 of positive sent for WGS tests (median)
 
 # Simulation of the sampling distribution
 
-nsim <- 1000
-yk <- rep(0,nsim)
-
-for(i in 1:nsim){
-print(i)
-    x <- rep("Neg",N)
-  x[1:xk] <- "C"
-  x[(xk+1):(xk+xkm)] <- "Pos"
-
-  z <- sample(x, nk)
-
-  y <- sample(z[z!="Neg"], mk)
-
-  yk[i] <- sum(y=="C")
-
-}
-
-dyk <- t(as.matrix(table(yk)))/nsim
-dyk <- rbind(as.integer(colnames(dyk)), as.vector(dyk))
-
+yk <- cluster.sampling(N, xk, xkm, nk, mk, nsim = 10000)
 
 # Poisson approximation
 
-lam <- mk*(xk)/(xk+xkm)
+lam <- mk*xk/(xk+xkm)
 
-# Kullback-Leibler divergence
+# Kullback-Leibler divergence between sampling distribution and plot
 
 KL <- kl(dyk, lam)
-plot(dyk[1,],dyk[2,], xlab = "Number", ylab = "Probability")
-points(dyk[1,],dpois(dyk[1,],lam), col = "red")
+plot(dyk[,1],dyk[,2], xlab = "Number", ylab = "Probability")
+points(dyk[,1],dpois(dyk[,1],lam), col = "red")
 legend("topright", pch = c(1,1), col = c("black", "red"), 
        legend = c("Sample","Poisson"), bty = "n")
-text(8,0.2,as.character(round(KL,5)))
+title(paste("KLD = ", as.character(round(KL,5))))
