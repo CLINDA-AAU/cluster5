52a5b7d86179316daf4bb2ad8952380ee53e034f
Martin Bøgsted <jywh@aas.its.nja.dk>
2021-02-10 12:52:15 +0100

    Intervention study

--- R/KLDivergence.R
+++ R/KLDivergence.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-# Double sampling of specific cluster
+# Double sampling function of specific cluster
 
 cluster.sampling <- function(N, xk, xkm, nk, mk, nsim = 100){
 
@@ -22,41 +22,60 @@ cluster.sampling <- function(N, xk, xkm, nk, mk, nsim = 100){
 }
 
 
-# Kullback-Leibler divergence between a discrete density dy 
-# and the Poisson distribution with parameter lamda
+# Kullback-Leibler divergence between a discrete densities dy 
+# and dz
 
-kl <- function(dy, lambda){
+kl <- function(dy, dz){
   
-  sum(dy[,2]*log(dy[,2]/dpois(dy[,1],lambda)))
+  sum(dy[,2]*log(dy[,2]/dz[,2]))
 
 }
-
 
 # Read sequence data from the Danish Cluster 5 case
 
 seqdata <- read.csv("Data/rn_cluster5_data.csv", sep = ";")
 
-N   <- 600000                 # Population size (North Denmark Region)
+N   <- 589148                 # Population size (North Denmark Region)
 xkmm <- 0.006*N               # 0.6% of the population is positive (median)
 xk  <- round(0.006*N*0.08)    # 8% of infected with specific variant
 xkm <- xkmm - xk              # Number of infected with non-specific variant
+pk <- 0.28                    # Ratio of positives WGS tested
 
 nk  <- 17000   # Numer of PCR tests (median)
-mk  <- round(0.28*nk*(xk+xkm)/N)  # 0.28 of positive sent for WGS tests (median)
+mk  <- round(pk*nk*(xk+xkm)/N)  # 0.28 of positive sent for WGS tests (median)
 
 # Simulation of the sampling distribution
 
-yk <- cluster.sampling(N, xk, xkm, nk, mk, nsim = 10000)
+nsim <- 10000
+yk <- cluster.sampling(N, xk, xkm, nk, mk, nsim = nsim)
+dyk <- as.data.frame(table(yk))
+dyk$yk <- as.integer(levels(dyk$yk))
+dyk$Freq <- dyk$Freq/nsim
 
 # Poisson approximation
 
 lam <- mk*xk/(xk+xkm)
+dzkpois <- data.frame(yk=dyk$yk, Freq=dpois(dyk[,1],lam))
+KLpois <- kl(dyk, dzk)
+
+# Binomial approximation
+
+dzk <- data.frame(yk=dyk$yk, Freq =dbinom(dyk[,1], mk, xk/(xk+xkm)))
+KLbinom1 <- kl(dyk, dzk)
+
+# Binomial approximation
+
+dzk <- data.frame(yk=dyk$yk, Freq =dbinom(dyk[,1], pk*nk, xk/N))
+KLbinom2 <- kl(dyk, dzk)
 
 # Kullback-Leibler divergence between sampling distribution and plot
 
-KL <- kl(dyk, lam)
 plot(dyk[,1],dyk[,2], xlab = "Number", ylab = "Probability")
 points(dyk[,1],dpois(dyk[,1],lam), col = "red")
-legend("topright", pch = c(1,1), col = c("black", "red"), 
-       legend = c("Sample","Poisson"), bty = "n")
-title(paste("KLD = ", as.character(round(KL,5))))
+points(dyk[,1],dbinom(dyk[,1], mk, xk/(xk+xkm)), col = "blue")
+points(dyk[,1],dbinom(dyk[,1], pk*nk, xk/N), col = "green")
+legend("topright", pch = c(1,1), col = c("black", "red","blue", "green"), 
+       legend = c("Sample","Poisson", "Binom1", "Binom2"), bty = "n")
+title(paste("KLD = ", as.character(round(KLpois,5)),",",as.character(round(KLbinom1,5)),"",as.character(round(KLbinom2,5))))
+
+
---
+++ R/Simulations.R
@@ -0,0 +1,191 @@
+#
+# This is an R-script, calculating the results of various intervention strategies, 
+# the Danish Mink Case, and Kullback-Leibler Divergence of approximations.
+#
+
+library(expm)
+library(ggplot2)
+library(tidyverse)
+library(patchwork)
+library(Hmisc)
+
+constructQ <- function(MaxI, gamma, beta){
+  Q1 = matrix(0, MaxI, MaxI)
+  delta = row(Q1) - col(Q1)
+  Q1[delta == 1] <- (1:(MaxI-1))*gamma
+  Q1[delta == -1] <- (0:(MaxI-2))*beta
+  diag(Q1) <- -1*apply(Q1,1,sum)
+  EQ1 = expm(Q1)
+  return(EQ1)
+  
+}
+
+runCalc <- function(  N1 = 300000, N2=N1,
+                      n1 = 10000,  n2=n1,
+                      gamma1=1/3.4,gamma2=gamma1,
+                      R1=1.2, R2=R1,
+                      MaxI=100,
+                      NumDays=28,
+                      TimeIntervention = Inf,
+                      IniMean=12,
+                      UseMeas="Yes",
+                      IniProb="3",
+                      Threshold = 0.9){
+    
+    
+    # Definitions etc.
+    beta1 = R1*gamma1; #Birth rate
+#    n1 = matrix(1, nrow = NumDays, ncol =1)*n1
+
+    beta2 = R2*gamma2; #Birth rate
+#    n2 = matrix(1, nrow = NumDays, ncol =1)*n2
+
+  if(TimeIntervention < NumDays){    
+    n = rbind(matrix(n1, nrow = TimeIntervention-1, ncol=1),
+              matrix(n2, nrow = NumDays-TimeIntervention+1, ncol=1))}else
+    n = matrix(n1, nrow = NumDays, ncol=1)
+    
+        
+    EQ1 = constructQ(MaxI, beta1, gamma1)
+    EQ2 = constructQ(MaxI, beta2, gamma2)
+        
+    # Measurements
+    Y = matrix(0, nrow = NumDays, ncol = 1); # Assume no Cluster5
+    Y[20,1]= 0*1;                            # Include a measurements of Cluster5
+    if (UseMeas == "No"){
+      Y= Y*NaN;
+    }
+    
+    # Initial probability for states
+    P = matrix(0, nrow = MaxI, ncol = 1);
+    switch(IniProb,                      
+           "1" = {P[IniMean,1] <- 1 },                       # Kronecker delta
+           # "2" = {P[Nlow:Nhigh] <- 1/(Nhigh-Nlow+1) },       # Uniform
+           "3" = {P[,1] <- dpois( 1:(MaxI), IniMean) }       # Poisson
+    )
+    
+    
+    # nstates x ndays container, holding day by day state aposteriori probabilities.
+    PP = matrix(0, nrow = MaxI, ncol = NumDays); 
+    
+    
+    for(i in 1:NumDays){
+      # Measurement update
+      if (!is.nan(Y[i])){ # If there is a measurement
+        P = P*dbinom(Y[i]*as.vector(matrix(1,MaxI,1)),
+                     as.vector(n[i]*matrix(1,MaxI,1)),
+                     (1:(MaxI)-1)/N1);
+        P= P/sum(P);
+      }
+      PP[,i] = P;                # PP does not include the initial PDF
+      # Time update;
+      if(i<TimeIntervention){
+        EQ = EQ1}else{
+        EQ = EQ2
+      }
+      
+      P = t(EQ)%*%P;
+      
+      # Correction for the error due to truncating number of infected to MaxI
+      P = P/sum(P); 
+    }
+    return(PP)
+  }
+
+## Simulation of increased WGS testing activity and restrictions
+
+NumDays = 90
+nk      = 10000
+pk <- c(0.25, 0.5, 0.75, 1.0)
+R <- c(0.5, 0.75, 1.0, 1.25, 1.5)
+Threshold = c(0.85,0.90,0.95)
+
+for(j in 1:(length(R))){
+  for(i in 1:(length(pk))){
+  PP <- runCalc(N1 = N,
+                n1 = pk[i]*nk,
+                gamma1=0.3,
+                R1=R[j],
+                MaxI=100,
+                NumDays=NumDays,
+                TimeIntervention=Inf,
+                IniMean=12,
+                UseMeas="Yes",
+                IniProb="3", Threshold = Threshold)
+  tmp <- data.frame(Day=1:NumDays, Prob=PP[1,], Ratio = pk[i], R = R[j])
+  if(i==1 && j==1){
+    res <- tmp}else 
+      res <- rbind(res, tmp)
+  }
+}
+
+res.testing <- res[res$R==1,] 
+
+res.testing$Ratio <- as.factor(as.character(res.testing$Ratio))
+  
+p.testing <- ggplot(res.testing, aes(x=Day, y=Prob)) + 
+  geom_point(res.testing, mapping = aes(x=Day, y=Prob, color=Ratio)) + 
+  xlab("Day") + ylab("Probability of extinction") + 
+  geom_hline(yintercept=0.85, linetype="dashed", color = "grey", size = 1) + 
+  geom_hline(yintercept=0.9, linetype="dashed", color = "grey", size = 1) +
+  geom_hline(yintercept=0.95, linetype="dashed", color = "grey", size = 1) +
+  scale_color_brewer(palette="YlOrRd") +
+  plot_annotation(tag_levels = "A")
+p.testing$labels$colour <- "WGS Ratio"
+
+res.restrictions <- res[res$Ratio==0.25,] 
+
+res.restrictions$R <- as.factor(as.character(res.restrictions$R))
+
+p.restrictions <- ggplot(res.restrictions, aes(x=Day, y=Prob)) + 
+  geom_point(res.restrictions, mapping = aes(x=Day, y=Prob, color=R)) + 
+  xlab("Day") + ylab("Probability of extinction") + 
+  geom_hline(yintercept=0.85, linetype="dashed", color = "grey", size = 1) + 
+  geom_hline(yintercept=0.9, linetype="dashed", color = "grey", size = 1) +
+  geom_hline(yintercept=0.95, linetype="dashed", color = "grey", size = 1) +
+  scale_color_brewer(palette="YlOrRd") +
+  plot_annotation(tag_levels = "B")
+p.restrictions$labels$colour <- "R"
+
+# Figure
+
+pdf("Interventions.pdf")
+
+p.testing / p.restrictions
+
+dev.off()
+
+# Table
+
+thres <- function(x){
+  match(FALSE,x<0.9)
+}
+
+thresholds <- res %>% 
+  group_by(Ratio, R) %>%
+  summarise(Cut = thres(Prob))
+
+rownames(thresholds) <- NULL
+
+latex(thresholds,file="days.tex", 
+      caption = "Days to thresholds for various testing and restriction strategies.", 
+      label = "tab:intervention", rowname=NULL)
+
+
+## Danish mink case
+
+# Map of Denmark
+
+# Intervention analysis
+
+N   <- 589148                 # Population size (North Denmark Region)
+xkmm <- 0.006*N               # 0.6% of the population is positive (median)
+xk  <- round(0.006*N*0.08)    # 8% of infected with specific variant
+xkm <- xkmm - xk              # Number of infected with non-specific variant
+pk <- 0.28                    # Ratio of positives WGS tested
+
+nk  <- 17000   # Numer of PCR tests (median)
+mk  <- round(pk*nk*(xk+xkm)/N)  # 0.28 of positive sent for WGS tests (median)
+
+NumDays = 80
+
