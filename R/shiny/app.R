30f3b3107bbb7f4a181de1ddd8ba58cc419b09b9
Rasmus Froberg Brøndum <y12c@aas.its.nja.dk>
2021-02-15 09:52:49 +0100

    Separate functions, create markdown

--- R/shiny/app.R
+++ R/shiny/app.R
@@ -13,6 +13,8 @@ library(ggplot2)
 library(tidyverse)
 library(patchwork)
 
+source("functions.R")
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     
@@ -74,13 +76,10 @@ ui <- fluidPage(
                         value = 0.9),
             radioButtons("IniProb",
                         "IniProb:",
-                        choices = c("Kronecker delta" = "1",
-                                    #"Uniform" = "2",
-                                    "Poisson" = "3"),
-                        selected = 3),
-            radioButtons("UseMeas",
-                         "UseMeas",
-                         choices = c("Yes","No"))
+                        choices = c("Kronecker delta" = "Kronecker",
+                                    #"Uniform" = "Uniform",
+                                    "Poisson" = "Poisson"),
+                        selected = "Poisson"),
             
         ),
         
@@ -95,104 +94,14 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    thres <- function(x, threshold){
-        match(FALSE,x<threshold)
-    }
-    
-    constructQ <- function(MaxI, gamma, beta){
-        Q1 = matrix(0, MaxI, MaxI)
-        delta = row(Q1) - col(Q1)
-        Q1[delta == 1] <- (1:(MaxI-1))*gamma
-        Q1[delta == -1] <- (0:(MaxI-2))*beta
-        diag(Q1) <- -1*apply(Q1,1,sum)
-        EQ1 = expm(Q1)
-        return(EQ1)
-        
-    }
-    
-    runCalc <- function(  N1 = 300000, N2=N1,
-                          n1 = 10000,  n2=n1,
-                          gamma1=1/3.4,gamma2=gamma1,
-                          R1=1.2, R2=R1,
-                          MaxI=100,
-                          NumDays=28,
-                          Y = rep(0,NumDays),
-                          TimeIntervention = Inf,
-                          IniMean=12,
-                          UseMeas="Yes",
-                          IniProb="3",
-                          Threshold = 0.9){
-        
-        
-        # Definitions etc.
-        beta1 = R1*gamma1; #Birth rate
-        #    n1 = matrix(1, nrow = NumDays, ncol =1)*n1
-        
-        beta2 = R2*gamma2; #Birth rate
-        #    n2 = matrix(1, nrow = NumDays, ncol =1)*n2
-        
-        if(TimeIntervention < NumDays){    
-            n = rbind(matrix(n1, nrow = TimeIntervention-1, ncol=1),
-                      matrix(n2, nrow = NumDays-TimeIntervention+1, ncol=1))}else
-                          n = matrix(n1, nrow = NumDays, ncol=1)
-                      
-                      
-                      EQ1 = constructQ(MaxI, beta1, gamma1)
-                      EQ2 = constructQ(MaxI, beta2, gamma2)
-                      
-                      # Measurements
-                      #    Y = matrix(0, nrow = NumDays, ncol = 1); # Assume no Cluster5
-                      #    Y[20,1]= 0*1;                            # Include a measurements of Cluster5
-                      #    if (UseMeas == "No"){
-                      #      Y= Y*NaN;
-                      #    }
-                      
-                      # Initial probability for states
-                      P = matrix(0, nrow = MaxI, ncol = 1);
-                      switch(IniProb,                      
-                             "1" = {P[IniMean,1] <- 1 },                       # Kronecker delta
-                             # "2" = {P[Nlow:Nhigh] <- 1/(Nhigh-Nlow+1) },       # Uniform
-                             "3" = {P[,1] <- dpois( 1:(MaxI), IniMean) }       # Poisson
-                      )
-                      
-                      
-                      # nstates x ndays container, holding day by day state aposteriori probabilities.
-                      PP = matrix(0, nrow = MaxI, ncol = NumDays); 
-                      
-                      
-                      for(i in 1:NumDays){
-                          # Measurement update
-                          if (!is.nan(Y[i])){ # If there is a measurement
-                              P = P*dbinom(Y[i]*as.vector(matrix(1,MaxI,1)),
-                                           as.vector(n[i]*matrix(1,MaxI,1)),
-                                           (1:(MaxI)-1)/N1);
-                              P= P/sum(P);
-                          }
-                          PP[,i] = P;                # PP does not include the initial PDF
-                          # Time update;
-                          if(i<TimeIntervention){
-                              EQ = EQ1}else{
-                                  EQ = EQ2
-                              }
-                          
-                          P = t(EQ)%*%P;
-                          
-                          # Correction for the error due to truncating number of infected to MaxI
-                          P = P/sum(P); 
-                      }
-                      return(PP)
-    }
-        
-        output$distPlot <- renderPlot({
+    output$distPlot <- renderPlot({
                 PP <- runCalc(N1=input$N1, N2=input$N1,
                               n1=input$n1, n2=input$n1,
                               gamma1=input$gamma1, gamma2=input$gamma1,
                               R1=input$R1, R2=input$R1,
                               MaxI = input$MaxI,
                               NumDays=input$NumDays,
-                              Threshold = input$Threshold,
                               IniMean=input$IniMean,
-                              UseMeas=input$UseMeas,
                               IniProb=input$IniProb)
                 
                 gg <- data.frame(ts(PP)) %>% 
@@ -233,7 +142,7 @@ server <- function(input, output) {
                 
                 prob_days_plot <- gg %>% filter(rowname == 1) %>% 
                     ggplot(aes(x = NumDays, y = cs)) +
-                    geom_line(color = "#221a52", size = 2) +
+                    geom_line(color = "#211a52", size = 2) +
                     xlab("Day") + 
                     ylab("Probability of extinction") +
                     ylim(c(0,1)) +
---
+++ R/shiny/functions.R
@@ -0,0 +1,78 @@
+## Days to threhold for extinction
+thres <- function(x, threshold){
+  match(FALSE,x<threshold)
+}
+
+
+## Construction of rate matrices
+constructQ <- function(MaxI, gamma, beta){
+  Q1 = matrix(0, MaxI, MaxI)
+  delta = row(Q1) - col(Q1)
+  Q1[delta == 1] <- (1:(MaxI-1))*gamma
+  Q1[delta == -1] <- (0:(MaxI-2))*beta
+  diag(Q1) <- -1*apply(Q1,1,sum)
+  EQ1 = expm(Q1)
+  return(EQ1)
+  
+}
+
+
+## Calculation of a posteriori probabilities
+runCalc <- function(  N1 = 300000, N2=N1,
+                      n1 = 10000,  n2=n1,
+                      gamma1=1/3.4, gamma2=gamma1,
+                      R1=1.2, R2=R1,
+                      MaxI=100,
+                      TimeIntervention = Inf,
+                      NumDays=28,
+                      Y = rep(0,NumDays),
+                      IniMean=12,
+                      IniProb="Kronecker"
+){
+  # Set the birth rate corresponding to the reproduction rate and recovery time    
+  beta1 = R1*gamma1; # Before intervention
+  beta2 = R2*gamma2; # After intervention
+  
+  if(TimeIntervention < NumDays){    
+    n = rbind(matrix(n1, nrow = TimeIntervention-1, ncol=1),
+              matrix(n2, nrow = NumDays-TimeIntervention+1, ncol=1))}else
+                n = matrix(n1, nrow = NumDays, ncol=1)
+              
+              # Transition probability matrix
+              
+              EQ1 = constructQ(MaxI+1, beta1, gamma1)
+              EQ2 = constructQ(MaxI+1, beta2, gamma2)
+              
+              # Initial probability for states
+              P = matrix(0, nrow = MaxI+1, ncol = 1);
+              switch(IniProb,                      
+                     "Kronecker" = {P[IniMean+1,1] <- 1 },                # Kronecker delta
+                     "Uniform" = {P[Nlow:Nhigh,1] <- 1/(Nhigh-Nlow+1) },  # Uniform
+                     "Poisson" = {P[,1] <- dpois(0:MaxI, IniMean) }       # Poisson
+              )
+              
+              # nstates x ndays container, holding day by day state a posterior probabilities.
+              PP = matrix(0, nrow = MaxI+1, ncol = NumDays); 
+              
+              for(i in 1:NumDays){
+                # Measurement update
+                if (!is.nan(Y[i])){
+                  P = P*dbinom(Y[i]*as.vector(matrix(1,MaxI+1,1)),
+                               as.vector(n[i]*matrix(1,MaxI+1,1)),
+                               (1:(MaxI+1)-1)/N1)
+                  P= P/sum(P)
+                }
+                PP[,i] = P;                # PP does not include the initial PDF
+                # Time update;
+                if(i<TimeIntervention){
+                  EQ = EQ1}else{
+                    EQ = EQ2
+                  }
+                
+                P = t(EQ)%*%P;
+                
+                # Correction for the error due to truncating number of infected to MaxI
+                P = P/sum(P); 
+              }
+              return(PP)
+}
